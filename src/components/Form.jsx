da34ce0c452ca30db02bd4a0271c6126b87d2e38
fautomationhome <fautomationhome@gmail.com>
2024-10-18 19:38:37 -0500

    add some functions in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,68 +1,107 @@
+import firebaseConfig from '../config/firebaseConfig'
+import { useState } from 'react'
+
 function Form() {
-    return(
+    const cliente = {
+        name: '',
+        address: '',
+        phone: '',
+        web: '',
+        email: ''
+    }
+
+    const [client, setClient] = useState(cliente)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setClient({ ...client, [name]: value })
+    }
+
+    const saveClient = (e) => {
+        e.preventDefault()
+        console.log(client)
+    }
+
+    return (
         <div>
             <div className="shadow-sm rounded-sm mt-5">
                 <h4 className="text-center">Registro de clientes</h4>
-                <form action="" className="p-4">
-                    <div className="form-floating">
-                        <input 
+                <form action="" className="p-4" onSubmit={saveClient}>
+                    <div className="flex flex-col">
+                        <label htmlFor="floatingInput">Ingresar Nombre</label>
+                        <input
                             type="text"
-                            className="form-control mt2 mb-2"
+                            className="border border-blue-500 mt2 mb-2"
                             id="floatingInput"
                             placeholder="Ingresar Nombre"
-                            name="nombre"
-                            required />
-                        <label htmlFor="floatingInput">Ingresar Nombre</label>
+                            name="name"  
+                            onChange={handleChange}
+                            value={client.name}
+                            required
+                        />
                     </div>
-                    <div className="form-floating">
-                        <input 
+                    <div className="flex flex-col">
+                        <label htmlFor="floatingInput">Ingresar Dirección</label>
+                        <input
                             type="text"
-                            className="form-control mt2 mb-2"
+                            className="border border-blue-500 mt2 mb-2"
                             id="floatingInput"
                             placeholder="Ingresar Dirección"
-                            name="Dirección"
-                            required />
-                        <label htmlFor="floatingInput">Ingresar Dirección</label>
+                            name="address" 
+                            onChange={handleChange}
+                            value={client.address}
+                            required
+                        />
                     </div>
-                    <div className="form-floating">
-                        <input 
+                    <div className="flex flex-col">
+                        <label htmlFor="floatingInput">Ingresar teléfono</label>
+                        <input
                             type="text"
-                            className="form-control mt2 mb-2"
+                            className="border border-blue-500 mt2 mb-2"
                             id="floatingInput"
                             placeholder="Ingresar teléfono"
-                            name="teléfono"
-                            required />
-                        <label htmlFor="floatingInput">Ingresar teléfono</label>
+                            name="phone" 
+                            onChange={handleChange}
+                            value={client.phone}
+                            required
+                        />
                     </div>
                     <div>
                         <label>Elegir Foto de perfil</label>
-                        <input 
+                        <input
                             type="file"
                             id="foto"
-                            className="form-control"
-                            required />
+                            className="border border-blue-500"
+                            // required
+                        />
                     </div>
-                    <div className="form-floating">
-                        <input 
+                    <div className="flex flex-col">
+                        <label htmlFor="floatingInput">Ingresar web</label>
+                        <input
                             type="text"
-                            className="form-control mt2 mb-2"
+                            className="border border-blue-500 mt2 mb-2"
                             id="floatingInput"
                             placeholder="Ingresar web"
                             name="web"
-                            required />
-                        <label htmlFor="floatingInput">Ingresar web</label>
+                            onChange={handleChange}
+                            value={client.web}
+                            required
+                        />
                     </div>
-                    <div className="form-floating">
-                        <input 
+                    <div className="flex flex-col border">
+                        <label htmlFor="floatingInput">Ingresar email</label>
+                        <input
                             type="email"
-                            className="form-control mt2 mb-2"
+                            className="border border-blue-500 mt2 mb-2"
                             id="floatingInput"
                             placeholder="Ingresar email"
                             name="email"
-                            required />
-                        <label htmlFor="floatingInput">Ingresar email</label>
+                            onChange={handleChange}
+                            value={client.email}
+                            required
+                        />
                     </div>
-                    <button>Enviar</button>                
+                    <button className="bg-blue-400 rounded-lg w-28 h-10">Enviar</button>
                 </form>
             </div>
         </div>
